f38df1fdb73520d6c8e15a8fd6fa569b22235759
William Kategianes <wKategianes@gmail.com>
2022-08-26 12:12:00 -0700

    Added Code Page
    Updated app.html
    Updated code.html
    Updated index.html
    Updated site.css
    Updated site.js
    Fixed Typos

--- js/site.js
+++ js/site.js
@@ -1,4 +1,38 @@
-//Call Hello World
-function helloWorld(){
-    alert("Hello World");
+//Get the string from the page
+//controller function
+function getValue(){
+
+    document.getElementById("alert").classList.add("invisible");
+    
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+//Reverse the string
+//logic function
+function reverseString(userString){
+
+    let revString = [];
+
+    //reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+
+        revString += userString[index];        
+    }
+
+    return revString;
+}
+
+//Display the reversed string to the user
+//view function
+function displayString(revString){
+
+    //write the message to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
